4eac7a20bb8fea64b8d740e00ade691dfa645783
Palden D. Titung <paldendorjetitung@gmail.com>
2026-04-26 01:37:22 +0545

    style: add framer animation in post listing

--- frontend/src/components/post/PostListing.tsx
+++ frontend/src/components/post/PostListing.tsx
@@ -1,11 +1,19 @@
 import { mockPosts } from "../../mocks/mockPosts";
 import PostCard from "./PostCard";
+import { motion } from "framer-motion";
 
 const PostListing = () => {
   return (
     <div className="flex flex-col space-y-6">
-      {mockPosts.map((post) => (
-        <PostCard key={post.id} post={post} />
+      {mockPosts.map((post, index) => (
+        <motion.div
+          key={post.id}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, delay: index * 0.08 }}
+        >
+          <PostCard post={post} />
+        </motion.div>
       ))}
     </div>
   );
