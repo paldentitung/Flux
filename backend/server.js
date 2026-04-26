36c9092ad7e3bb83eabf4ad9b5050a1761630236
Palden D. Titung <paldendorjetitung@gmail.com>
2026-04-26 18:03:48 +0545

    feat: setup server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Hello from server");
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 7777;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
