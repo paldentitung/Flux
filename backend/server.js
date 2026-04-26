ebf55e24da708b54b2f4a8d3d15b3bcb22f808cd
Palden D. Titung <paldendorjetitung@gmail.com>
2026-04-26 18:18:27 +0545

    feat: connect to mongodb using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
 
 dotenv.config({ path: ".env.local" });
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 7777;
 
 app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
