d6cdf96a42ade1cd5d91753aabb52c6bc618bb4f
Palden D. Titung <paldendorjetitung@gmail.com>
2026-05-02 17:29:36 +0545

    feat: create a fetching wrapper and implemented it in post service

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,18 @@
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+const request = async (url: string, options?: RequestInit, auth = true) => {
+  const res = await fetch(`${API_BASE_URL}${url}`, {
+    ...(auth && { credentials: "include" }),
+    ...options,
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error(data.message || "Something went wrong");
+  }
+
+  return data;
+};
+
+export default request;
--- frontend/src/services/postsService.ts
+++ frontend/src/services/postsService.ts
@@ -1,83 +1,24 @@
-import toast from "react-hot-toast";
+import request from "./api";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
-
-export const getPosts = async () => {
-  try {
-    const res = await fetch(`${API_BASE_URL}/posts`, {
-      method: "GET",
-      credentials: "include",
-    });
-    const data = await res.json();
-    if (!res.ok) {
-      throw new Error(data.message || "somethink went wrong");
-    }
-    return data;
-  } catch (error: any) {
-    toast.error(error.message);
-  }
+export const getPosts = () => {
+  return request("/posts");
 };
-export const createPost = async (formData: FormData): Promise<any> => {
-  try {
-    const res = await fetch(`${API_BASE_URL}/posts`, {
-      method: "POST",
-      credentials: "include",
-      body: formData,
-    });
-
-    const data = await res.json();
-
-    if (!res.ok) {
-      throw new Error(data.message || "something went wrong");
-    }
-
-    return data;
-  } catch (error: any) {
-    toast.error(error.message);
-  }
+export const createPost = (formData: FormData) => {
+  return request("/posts", {
+    method: "POST",
+    body: formData,
+  });
 };
 
-export const deletePost = async (postId: string) => {
-  try {
-    const res = await fetch(`${API_BASE_URL}/posts/${postId}`, {
-      method: "DELETE",
-      credentials: "include",
-    });
-
-    const data = await res.json();
-
-    if (!res.ok) {
-      throw new Error(data.message || "Something went wrong");
-    }
-
-    return data;
-  } catch (error) {
-    if (error instanceof Error) {
-      toast.error(error.message);
-      throw error;
-    }
-  }
+export const deletePost = (postId: string) => {
+  return request(`/posts/${postId}`, {
+    method: "DELETE",
+  });
 };
 
-export const updatePost = async (postId: string, formData: FormData) => {
-  try {
-    const res = await fetch(`${API_BASE_URL}/posts/${postId}`, {
-      method: "PATCH",
-      credentials: "include",
-      body: formData,
-    });
-
-    const data = await res.json();
-
-    if (!res.ok) {
-      throw new Error(data.message || "Something went wrong");
-    }
-
-    return data;
-  } catch (error) {
-    if (error instanceof Error) {
-      toast.error(error.message);
-      throw error;
-    }
-  }
+export const updatePost = (postId: string, formData: FormData) => {
+  return request(`/posts/${postId}`, {
+    method: "PATCH",
+    body: formData,
+  });
 };
