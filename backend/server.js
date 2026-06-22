b010083db8fb1135af1f669a9ad27100b4340988
Palden D. Titung <paldendorjetitung@gmail.com>
2026-06-22 15:52:30 +0545

    feat: add Socket.IO setup for real-time communication

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,41 @@
 import dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
 
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
+const httpServer = createServer(app);
+console.log(process.env.CLIENT_URL);
+export const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`Client connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`Client disconnected: ${socket.id}`);
+  });
+});
+
 const PORT = process.env.PORT || 7777;
 
-connectDB();
+const startServer = async () => {
+  try {
+    await connectDB();
 
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+    httpServer.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+startServer();
